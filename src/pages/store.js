c4519037ec81e27f1ec60007b654d4a7fc48c91f
hajadelmas <52510008+hajadelmas@users.noreply.github.com>
2020-12-15 17:52:11 +0100

    category query

--- src/pages/store.js
+++ src/pages/store.js
@@ -6,6 +6,8 @@ import SEO from "../components/seo"
 import StarRatingComponent from 'react-star-rating-component';
 import { graphql } from "gatsby";
 
+import { Accordion, Card } from "react-bootstrap"
+
 class IndexPost extends React.Component {
   constructor(props) {
     super(props);
@@ -41,8 +43,8 @@ class IndexPost extends React.Component {
 
     return (
       <React.Fragment>
-        <div className="row product-main" onScroll={this.onScrollEvent}>
-          {data.data.allContentfulProduct.edges.slice(0, NoOfPost).map(items => (
+        {/* <div className="row product-main" onScroll={this.onScrollEvent}>
+          {data.data.platTags.edges.slice(0, NoOfPost).map(items => (
             <div className="Catalogue__item col-sm-12 col-md-6 col-lg-4" key={items.node.id}>
               <div className="details_List">
                 {items.node.image === null ? <div className="no-image">No Image</div> : <Img sizes={items.node.image.fixed} />}
@@ -61,25 +63,87 @@ class IndexPost extends React.Component {
                     <div className="col-sm-4 align-self-center">
                       <span className="indexPrice">{items.node.price} €</span>
                     </div>
-                    <div className="col-sm-8 text-right align-self-center">
-                      {/* <a
-                        // href="#"
-                        className="Product snipcart-add-item"
-                        data-item-id={items.node.slug}
-                        data-item-price={items.node.price}
-                        data-item-image={items.node.image === null ? "" : items.node.image.fixed.src}
-                        data-item-name={items.node.name}
-                        data-item-url={`/`}
-                      >
-                        <i className="fas fa-shopping-bag" />Add to Cart
-                    </a> */}
-                    </div>
                   </div>
                 </div>
               </div>
             </div>
           ))}
-        </div>
+        </div> */}
+
+        <Accordion defaultActiveKey="0">
+          <Card>
+            <Accordion.Toggle as={Card.Header} eventKey="0">
+              Les Plats
+            </Accordion.Toggle>
+            <Accordion.Collapse eventKey="0">
+              <Card.Body>
+                <div className="row product-main" onScroll={this.onScrollEvent}>
+                  {data.data.platTags.edges.slice(0, NoOfPost).map(items => (
+                    <div className="Catalogue__item col-sm-12 col-md-6 col-lg-4" key={items.node.id}>
+                      <div className="details_List">
+                        {items.node.image === null ? <div className="no-image">No Image</div> : <Img sizes={items.node.image.fixed} />}
+
+                        <div className="details_inner">
+                          <h2>
+                            <Link to={`/${items.node.slug}`}>{items.node.name}</Link>
+                          </h2>
+                          <StarRatingComponent
+                            name="rate1"
+                            starCount={5}
+                            value={items.node.rating}
+                          />
+                          <p>{items.node.details.childMarkdownRemark.excerpt}</p>
+                          <div className="row">
+                            <div className="col-sm-4 align-self-center">
+                              <span className="indexPrice">{items.node.price} €</span>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </Card.Body>
+            </Accordion.Collapse>
+          </Card>
+          <Card>
+            <Accordion.Toggle as={Card.Header} eventKey="1">
+              Les Desserts
+            </Accordion.Toggle>
+            <Accordion.Collapse eventKey="1">
+              <Card.Body>
+                <div className="row product-main" onScroll={this.onScrollEvent}>
+                    {data.data.dessertTags.edges.slice(0, NoOfPost).map(items => (
+                      <div className="Catalogue__item col-sm-12 col-md-6 col-lg-4" key={items.node.id}>
+                        <div className="details_List">
+                          {items.node.image === null ? <div className="no-image">No Image</div> : <Img sizes={items.node.image.fixed} />}
+
+                          <div className="details_inner">
+                            <h2>
+                              <Link to={`/${items.node.slug}`}>{items.node.name}</Link>
+                            </h2>
+                            <StarRatingComponent
+                              name="rate1"
+                              starCount={5}
+                              value={items.node.rating}
+                            />
+                            <p>{items.node.details.childMarkdownRemark.excerpt}</p>
+                            <div className="row">
+                              <div className="col-sm-4 align-self-center">
+                                <span className="indexPrice">{items.node.price} €</span>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+              </Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        </Accordion>
+
+
       </React.Fragment>
     );
   }
@@ -97,9 +161,62 @@ const IndexPage = data => (
 
 export default IndexPage
 
+// export const query = graphql`
+//   query StoreQuery {
+//     allContentfulProduct(filter:{tags:{eq:"plat"}}) {
+//       edges{
+//         node{
+//           id
+//           name
+//           slug
+//           rating
+//           image {
+//             fixed(width: 1000, height: 500) {
+//               width
+//               height
+//               src
+//               srcSet
+//             }
+//           }
+//           price
+//           details {
+//             childMarkdownRemark {
+//               excerpt(pruneLength: 140)
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `
+
 export const query = graphql`
   query StoreQuery {
-    allContentfulProduct{
+    dessertTags: allContentfulProduct(filter:{tags:{eq:"dessert"}}) {
+      edges{
+        node{
+          id
+          name
+          slug
+          rating
+          image {
+            fixed(width: 1000, height: 500) {
+              width
+              height
+              src
+              srcSet
+            }
+          }
+          price
+          details {
+            childMarkdownRemark {
+              excerpt(pruneLength: 140)
+            }
+          }
+        }
+      }
+    }
+    platTags: allContentfulProduct(filter:{tags:{eq:"plat"}}) {
       edges{
         node{
           id
