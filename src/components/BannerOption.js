baabd5a7863a739a5301dcfc6efc3af0f3a4bac4
hajadelmas <52510008+hajadelmas@users.noreply.github.com>
2020-12-28 17:23:51 +0100

    Ipad / Ipad pro

--- src/components/BannerOption.js
+++ src/components/BannerOption.js
@@ -1,3 +1,4 @@
+import { Link } from 'gatsby'
 import React, { Component } from 'react'
 import { Button } from 'react-bootstrap'
 
@@ -6,25 +7,25 @@ export default class BannerOption extends Component {
         return (
             <div className="container-fluid mt-0">
                 <div className="row d-flex justify-content-around p-4 bannerOptionRow">
-                    <div className="col-lg-3 col-sm-12 bannerOptionGeneral" data-sal="slide-up" data-sal-delay="50" data-sal-easing="ease-out-sine">
+                    <div className="col-lg-3 col-sm-12 col-md-6 bannerOptionGeneral" data-sal="slide-up" data-sal-delay="50" data-sal-easing="ease-out-sine">
                         <h2>Click and collect</h2>
                         <div className='bannerOptionGeneral_carre'>
-                            <p>Nos plats en vogue, les meilleurs desserts, pourquoi nos entrées sont si bonnes ? Retrouvez toute la carte en cliant sur le bouton ci dessous. </p>
-                            <Button className='bannerOption_btn'>Notre carte</Button>
+                            <p>Pour ne pas perdre le goût en ces temps difficiles, retrouvez nos meilleurs plats disponibles en click & collect près de chez vous. </p>
+                            <Link to='/Click' className=''>Click & collect</Link>
                         </div>
                     </div>
-                    <div className="col-lg-3 col-sm-12 bannerOptionGeneral" data-sal="slide-up" data-sal-delay="100" data-sal-easing="ease-out-sine">
+                    <div className="col-lg-3 col-sm-12 col-md-6 bannerOptionGeneral" data-sal="slide-up" data-sal-delay="100" data-sal-easing="ease-out-sine">
                         <h2>Nos restaurants</h2>
                         <div className='bannerOptionGeneral_carre'>
-                            <p>Nos plats en vogue, les meilleurs desserts, pourquoi nos entrées sont si bonnes ? Retrouvez toute la carte en cliant sur le bouton ci dessous. </p>
-                            <Button className='bannerOption_btn'>Notre carte</Button>
+                            <p>Nos restaurants et franchises sont à découvrir en cliquant sur le bouton ci-dessous. </p>
+                            <Link to='/restaurants'>Nos restaurants</Link>
                         </div>
                     </div>
-                    <div className="col-lg-3 col-sm-12 bannerOptionGeneral" data-sal="slide-up" data-sal-delay="150" data-sal-easing="ease-out-sine">
+                    <div className="col-lg-3 col-sm-12 col-md-6 bannerOptionGeneral" data-sal="slide-up" data-sal-delay="150" data-sal-easing="ease-out-sine">
                         <h2>Notre carte</h2>
                         <div className='bannerOptionGeneral_carre'>
                             <p>Nos plats en vogue, les meilleurs desserts, pourquoi nos entrées sont si bonnes ? Retrouvez toute la carte en cliant sur le bouton ci dessous. </p>
-                            <Button className='bannerOption_btn'>Notre carte</Button>
+                            <Link to='/store'>Notre carte</Link>
                         </div>
                     </div>
                 </div>
